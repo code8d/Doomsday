6817eedd25560f82bd40c1242a76c88564ad800a
code8d <code8d@gmail.com>
2022-06-28 19:35:30 +0500

    Added interactive for hamburger menu

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-console.log(window.innerWidth);
+'use strict';
+
+const hamburger = document.querySelector('.hamburger-block');
+const menu = document.querySelector('.header__nav-block');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.toggle('show');
+})
